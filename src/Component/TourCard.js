de4403a7be376254c4686b006f19c621b76f191f
Bruce Luk <lukusto@gmail.com>
2022-06-25 16:46:59 +0800

    add daisyUI

---
+++ src/Component/TourCard.js
@@ -0,0 +1,23 @@
+import React from "react"
+// import { Card } from "react-daisyui"
+// import { Button } from "react-daisyui"
+
+export default function TourCard({btn, des, title}) {
+  return (
+    <div class="card card-compact w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src="https://api.lorem.space/image/shoes?w=400&h=225"
+          alt="Shoes"
+        />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">{title}</h2>
+        <p>{des}</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">{btn}</button>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/Page/LangingPage.js
+++ src/Page/LangingPage.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import TourCard from '../Component/TourCard'
 
 export default function LangingPage() {
+
+  const content = [
+    {title: "Yes", des: "Buy now", btn: "Love"},
+    {title: "Yes1", des: "Buy now1", btn: "Love1"},
+    {title: "Yes2", des: "Buy now2", btn: "Love2"}
+  ]
+
+
   return (
-    <div>
+    <div className=''>
       LangingPage
+      <div className='grid grid-cols-2' >
+      {content.map((item, id) => {
+        return <TourCard btn={item.btn} title={item.title} des={item.des}></TourCard>
+      })}
+      </div>
+      
     </div>
   )
 }
